e32d195e815c4308d488e3af2ca239c21f1e6964
MoiseAganze <aganzedev@gmail.com>
2025-05-21 22:07:30 +0100

    new go

--- config/cors.js
+++ config/cors.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const corsOptions = {
-  origin: process.env.frontend,
+  origin: [process.env.FRONTEND_1, process.env.FRONTEND_2], // Tableau d'URLs
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
